31bc39eacbc4ffaa222e3674410b27116ec9b25e
zul110 <zul110@gmail.com>
2015-03-28 20:29:59 +0400

    small changes

--- TaxiSimulation/src/models/TaxiModel.java
+++ TaxiSimulation/src/models/TaxiModel.java
@@ -1,5 +1,13 @@
 package models;
 
 public class TaxiModel {
-
+	private String registrationNumber;
+	
+	public String getRegistrationNumber() {
+		return registrationNumber;
+	}
+	
+	public void setRegistrationNumber(String registrationNumber) {
+		this.registrationNumber = registrationNumber;
+	}
 }
---
+++ TaxiSimulation/src/views/Main.java
@@ -0,0 +1,13 @@
+package views;
+
+import controllers.SimulationController;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SimulationController sim = new SimulationController();
+		
+		sim.getTaxis();
+	}
+
+}
