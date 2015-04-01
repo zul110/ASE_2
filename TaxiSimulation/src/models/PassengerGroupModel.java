56caa7ca35aeb178874dc4c34fe6e714b155940b
ulqameh <ulqameh@R89TLZA-MS.NOE.Nokia.com>
2015-04-01 14:35:15 +0400

    Added Taxi and PassengerGroup Models

--- TaxiSimulation/src/models/PassengerGroupModel.java
+++ TaxiSimulation/src/models/PassengerGroupModel.java
@@ -1,5 +1,22 @@
 package models;
 
 public class PassengerGroupModel {
-
+	private int numberOfPassengers;
+	private String destination;
+	
+	public int getNumberOfPassengers() {
+		return numberOfPassengers;
+	}
+	
+	public void setNumberOfPassengers(int numberOfPassengers) {
+		this.numberOfPassengers = numberOfPassengers;
+	}
+	
+	public String getDestination() {
+		return destination;
+	}
+	
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
 }
---
+++ TaxiSimulation/src/models/TaxisListModel.java
@@ -0,0 +1,9 @@
+package models;
+
+import java.awt.List;
+
+public class TaxisListModel extends List {
+	public TaxisListModel() {
+		
+	}
+}
