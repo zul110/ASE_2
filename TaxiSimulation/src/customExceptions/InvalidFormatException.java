ec60b9e49a7e40d88d6aef7df02fd12fbae0ce0d
Vidhya <Vidhya@192.168.2.13>
2015-04-06 10:47:44 +0400

    Added

--- TaxiSimulation/src/customExceptions/InvalidFormatException.java
+++ TaxiSimulation/src/customExceptions/InvalidFormatException.java
@@ -1,5 +1,5 @@
 /**
- * Advanced Software Engineering - Stage 1 of Taxi Service Application 
+ * Advanced Software Engineering - Stage 2 of Taxi Simulation
  * @author Sreesha Damodaran, Vidhya Krishna, Zulqarnain Mehdi
  * This class describe custom exception - Invalid Format Exception
  * which checks the input text file details against the expected format
@@ -9,7 +9,7 @@ package customExceptions;
 /**
  * Custom Exception: InvalidFormatException
  * Thrown when data is encountered in an invalid format 
- * @author Zulqarnain
+ *  @author  Zulqarnain Mehdi,Sreesha Damodaran, Vidhya Krishna,
  *
  */
 public class InvalidFormatException extends Exception {
